372ea31d9e8aa8b4fc468a89b22ee74445fdb510
theimpactlab <ryan@theimpactlab.co.uk>
2025-04-16 12:15:18 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,20 @@ module.exports = {
         poppins: ["var(--font-poppins)", "sans-serif"],
       },
       colors: {
+        // Add back gray colors for compatibility
+        gray: {
+          50: "rgb(250 250 250)",
+          100: "rgb(244 244 245)",
+          200: "rgb(228 228 231)",
+          300: "rgb(212 212 216)",
+          400: "rgb(161 161 170)",
+          500: "rgb(113 113 122)",
+          600: "rgb(82 82 91)",
+          700: "rgb(63 63 70)",
+          800: "rgb(39 39 42)",
+          900: "rgb(24 24 27)",
+          950: "rgb(9 9 11)",
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
